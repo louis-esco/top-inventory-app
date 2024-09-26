b11186f568d336fda94c7829ae3ddbb5ba0d58f6
Louis Escoffier <louis.escoffier.bagrel@gmail.com>
2024-09-26 18:24:58 +0200

    Add styles

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
 
 app.use("/", indexRouter);
 
